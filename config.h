4c139610c53573cf7679e56cc3cd65f6dc865d1c
rgeorge04 <rgeorge.rabus@gmail.com>
2021-06-26 23:20:37 +0300

    changed firefox rule

--- config.h
+++ config.h
@@ -36,6 +36,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
+
 };
 
 /* layout(s) */
